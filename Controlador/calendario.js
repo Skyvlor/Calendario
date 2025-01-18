525fd8386ea7605fe717c38eaade34f121518824
Israel <israelalbornoz.r@gmail.com>
2025-01-18 20:28:36 -0300

    se agrega los dias de la semana y el titulo desde javascript

--- Controlador/calendario.js
+++ Controlador/calendario.js
@@ -6,6 +6,16 @@ Podria ser como una APi
 
 calendario = document.getElementById('calendario');
 let contenido = '<tr>';
+let diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+let Titulo = ['Calendario'];
+
+contenido += `<th colspan="7">${Titulo}</th></tr><tr>`;
+
+for (let i = 0; i < diasSemana.length; i++) {
+    contenido += `<th>${diasSemana[i]}</th>`;   
+};
+
+contenido+= '</tr><tr>';
 
 for (let i = 1; i <= 42; i++) {
     let dia = i <= 31 ? i : '';
