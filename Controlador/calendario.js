ce7122de4c928a18d013116d9ded52ce5a96c3d9
Israel <israelalbornoz.r@gmail.com>
2025-01-24 20:55:56 -0300

    agregados todos los meses

--- Controlador/calendario.js
+++ Controlador/calendario.js
@@ -2,29 +2,44 @@
 Podria ser como una APi 
 */
 
+/*tr es salto de linea 
+th es el titulo para el grid
+td es columna */
 
 
 calendario = document.getElementById('calendario');
 let contenido = '<tr>';
 let diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+let Meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto','Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 let Titulo = ['Calendario'];
 
 contenido += `<th colspan="7">${Titulo}</th></tr><tr>`;
 
-for (let i = 0; i < diasSemana.length; i++) {
-    contenido += `<th>${diasSemana[i]}</th>`;   
-};
 
-contenido+= '</tr><tr>';
+for (let i =0; i<Meses.length; i++){
+    
+    contenido += `<th>${Meses[i]}</th>`;
+    contenido+= '</tr><tr>';
 
-for (let i = 1; i <= 42; i++) {
-    let dia = i <= 31 ? i : '';
-    contenido += `<td>${dia}</td>`;
+    for (let i = 0; i < diasSemana.length; i++) {
+        contenido += `<th>${diasSemana[i]}</th>`;
+       
+    };
 
-    if (i % 7 === 0) {
-        contenido += '</tr><tr>';
+    contenido += '</tr><tr>';
+
+    for (let idia = 1; idia <= 42; idia++) {
+        
+        let dia = idia <= 31 ? idia : '';
+        contenido += `<td>${dia}</td>`;
+        
+    
+        if (idia % 7 === 0) {
+            contenido += '</tr><tr>';
+        }
     }
-}
+    contenido+= '</tr><tr>';
+};
 
 contenido += '</tr>';
 calendario.innerHTML = contenido;
