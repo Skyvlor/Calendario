c70cac04a43a2cf4190149fa9b3fd75d21d8f809
Israel <israelalbornoz.r@gmail.com>
2025-01-28 11:17:13 -0300

    se agrega estilos al calendario y se modificia el inicio del mes

--- Controlador/calendario.js
+++ Controlador/calendario.js
@@ -2,8 +2,8 @@
 Podria ser como una APi 
 */
 
-/*tr es salto de linea 
-th es el titulo para el grid
+/*th es la fila 
+tr es el titulo para el grid
 td es columna */
 
 
@@ -11,35 +11,40 @@ calendario = document.getElementById('calendario');
 let contenido = '<tr>';
 let diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
 let Meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto','Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-let Titulo = ['Calendario'];
+let Titulo = ['Calendario 2025'];
+let InicioDelMes = new Date(2025, 0, 1);
+console.log(InicioDelMes);
 
 contenido += `<th colspan="7">${Titulo}</th></tr><tr>`;
 
+for (let i = 0; i < Meses.length; i++) {
+    contenido += `<th colspan="7">${Meses[i]}</th></tr><tr>`;
 
-for (let i =0; i<Meses.length; i++){
-    
-    contenido += `<th>${Meses[i]}</th>`;
-    contenido+= '</tr><tr>';
-
-    for (let i = 0; i < diasSemana.length; i++) {
-        contenido += `<th>${diasSemana[i]}</th>`;
-       
-    };
+    for (let j = 0; j < diasSemana.length; j++) {
+        contenido += `<th>${diasSemana[j]}</th>`;
+    }
 
     contenido += '</tr><tr>';
 
-    for (let idia = 1; idia <= 42; idia++) {
-        
-        let dia = idia <= 31 ? idia : '';
-        contenido += `<td>${dia}</td>`;
-        
-    
-        if (idia % 7 === 0) {
+    let primerDiaMes = new Date(2025, i, 1).getDay();
+    primerDiaMes = (primerDiaMes + 6) % 7; // Ajustar para que Lunes sea el primer día
+
+    for (let k = 0; k < primerDiaMes; k++) {
+        contenido += '<td></td>';
+    }
+
+    let diasEnMes = new Date(2025, i + 1, 0).getDate();
+
+    for (let dia = 1; dia <= diasEnMes; dia++) {
+        contenido += `<td class='dia'>${dia}</td>`;
+
+        if ((dia + primerDiaMes) % 7 === 0) {
             contenido += '</tr><tr>';
         }
     }
-    contenido+= '</tr><tr>';
-};
+
+    contenido += '</tr><tr>';
+}
 
 contenido += '</tr>';
 calendario.innerHTML = contenido;
